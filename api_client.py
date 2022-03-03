e17ab0c40426caab11360693475f0c609c06cc4f
mkmacedo <markelp@protonmail.com>
2022-03-02 22:38:08 -0300

    api

--- api_client.py
+++ api_client.py
@@ -10,20 +10,20 @@ class API_Client():
 
     def connect(self):
 
-        params = {
-            'cif': 1191148.14,
-            'dataEntrada': "2021-10-03",
-            'dataSaida': "2021-10-05",
-            'pesoBruto': "579",
-            'pesoLiquido': "172.90"
-        }
+#        params = {
+#            'cif': 1191148.14,
+#            'dataEntrada': "2021-10-03",
+#            'dataSaida': "2021-10-05",
+#            'pesoBruto': "579",
+#            'pesoLiquido': "172.90"
+#        }
 
-        json = requests.get(self.url, params=params).json()
+        json = requests.get(self.url).json()
 
         return json
 
-#a = API_Client('http://127.0.0.1:5000/Rio?cif=1191148.14&dataEntrada="2021-10-03"&dataSaida="2021-10-05"&dataSaida="2021-10-05"&pesoBruto="579"&pesoLiquido="172.90"').result
-a = API_Client('http://127.0.0.1:5000/Rio').result
+a = API_Client('http://127.0.0.1:5000/Rio?cif=1191148.14&dataEntrada=2021-10-03&dataSaida=2021-10-05&dataSaida=2021-10-05&pesoBruto=579&pesoLiquido=172.90').result
+#a = API_Client('http://127.0.0.1:5000/Rio').result
 
 print(a)
 
